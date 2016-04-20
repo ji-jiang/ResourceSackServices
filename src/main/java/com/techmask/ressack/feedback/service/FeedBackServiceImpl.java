c418b6eb81781e27c72e861d6148af7c7c2415bd
xuewenke <xue379435450@163.com>
2016-04-20 23:51:31 +0800

    seven.commit 2016.4.20

--- src/main/java/com/techmask/ressack/feedback/service/FeedBackServiceImpl.java
+++ src/main/java/com/techmask/ressack/feedback/service/FeedBackServiceImpl.java
@@ -16,6 +16,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.techmask.ressack.core.util.ResultEntity;
 import com.techmask.ressack.feedback.domain.FeedBack;
 import com.techmask.ressack.feedback.repository.FeedBackRepository;
 
@@ -39,9 +40,108 @@ public class FeedBackServiceImpl implements FeedBackService {
 	* @throws 
 	*/
 	@Override
-	public List<FeedBack> loadAllFeedBack() {
+	public void loadAllFeedBack(ResultEntity resultEntity) {
 		
-		return feedBackRepository.loadAllFeedBack();
+		
+		List<FeedBack> feedBackList = feedBackRepository.loadAllFeedBack();
+		if(feedBackList == null){
+			resultEntity.setCode("201");
+			resultEntity.setMessage("获取失败");
+		}else if(feedBackList.size() == 0){
+			resultEntity.setCode("301");
+			resultEntity.setMessage("没有相应的数据");
+		}else{
+			resultEntity.setCode("0");
+			resultEntity.setMessage("获取数据成功");
+			resultEntity.setData(feedBackList);
+		}
+		return ;
 	}
+
+	/** 
+	* @Title: addFeedBack 
+	* @anthor Wenke
+	* @Description(描述这个方法) :  添加意见反馈
+	* @param name
+	* @param email
+	* @param content
+	* @param createBy
+	* @param updateBy
+	* @param resulMap
+	* @return  
+	* @throws 
+	*/
+	@Override
+	public void addFeedBack(String name,String email,String content,String createBy,String updateBy,ResultEntity resultEntity) {
+		
+		
+		FeedBack addFeedBack = new FeedBack(); 
+		addFeedBack.setName(name);
+		addFeedBack.setEmail(email);
+		addFeedBack.setContent(content);
+		addFeedBack.setCreatedBy(createBy);
+		addFeedBack.setUpdatedBy(updateBy);
+		int count = feedBackRepository.addFeedBack(addFeedBack);
+		if(count > 0){
+			resultEntity.setCode("0");
+			resultEntity.setMessage("添加成功");
+		}else{
+			resultEntity.setCode("201");
+			resultEntity.setMessage("添加失败");
+		}
+		return ;
+	}
+
+	/** 
+	* @Title: updateFeedBack 
+	* @anthor Wenke
+	* @Description(描述这个方法) : 修改意见反馈
+	* @param feedBackId
+	* @param content
+	* @param resulMap
+	* @return  
+	* @throws 
+	*/
+	@Override
+	public void updateFeedBack(String feedBackId ,String content,ResultEntity resultEntity) {
+		
+		FeedBack updateFeedBack = new FeedBack();
+		updateFeedBack.setFeedbackId(feedBackId);
+		updateFeedBack.setContent(content);
+		int count = feedBackRepository.updateFeedBack(updateFeedBack);
+		if(count > 0){
+			resultEntity.setCode("0");
+			resultEntity.setMessage("修改成功");
+		}else{
+			resultEntity.setCode("201");
+			resultEntity.setMessage("修改失败");
+		}
+		return ;
+	}
+
+	/** 
+	* @Title: deleteFeedBack 
+	* @anthor Wenke
+	* @Description(描述这个方法) :  根据id 删除意见反馈
+	* @param feedBackId
+	* @return  
+	* @throws 
+	*/
+	@Override
+	public void deleteFeedBack(String feedBackId,ResultEntity resultEntity) {
+		FeedBack deletFeedBack = new FeedBack();
+		deletFeedBack.setFeedbackId(feedBackId);
+		
+		int count = feedBackRepository.deleteFeedBack(deletFeedBack);
+		if(count > 0){
+			resultEntity.setCode("0");
+			resultEntity.setMessage("删除成功");
+		}else{
+			resultEntity.setCode("201");
+			resultEntity.setMessage("删除失败");
+		}
+		return ;
+	}
+
 
 }
