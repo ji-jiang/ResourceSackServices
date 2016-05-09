e5eadb8a9ba675b4bf681429f542c853943b9748
FisherChen <guiku2007@qq.com>
2016-05-09 13:42:35 +0800

    change weibo and qq oauth service.

--- src/main/java/com/techmask/ressack/usermanager/oauth/service/WeiboOAuthService.java
+++ src/main/java/com/techmask/ressack/usermanager/oauth/service/WeiboOAuthService.java
@@ -1,5 +1,8 @@
 package com.techmask.ressack.usermanager.oauth.service;
 
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.scribe.builder.api.DefaultApi20;
 import org.scribe.model.*;
 import org.scribe.oauth.OAuth20ServiceImpl;
@@ -17,6 +20,8 @@ public class WeiboOAuthService extends OAuth20ServiceImpl implements CustomOAuth
 	private final String authorizationUrl;
 	private static final String WEIBO_GET_TOKEN_INFO = "https://api.weibo.com/oauth2/get_token_info";
 	private static final String WEIBO_GET_USER_INFO = "https://api.weibo.com/2/users/show.json";
+	
+	private final Log logger = LogFactory.getLog(getClass());
 
 	public WeiboOAuthService(DefaultApi20 api, OAuthConfig config) {
 		super(api, config);
@@ -25,36 +30,40 @@ public class WeiboOAuthService extends OAuth20ServiceImpl implements CustomOAuth
 		this.authorizationUrl = getAuthorizationUrl(null);
 	}
 
-	
 	/***
-	 * you can get all weibo user info from "http://open.weibo.com/wiki/2/users/show";
-	***/
-	
-	
-	
+	 * you can get all weibo user info from
+	 * "http://open.weibo.com/wiki/2/users/show";
+	 ***/
+
 	@Override
 	public User getOAuthUser(Token accessToken) {
-		OAuthRequest getUidRequest = new OAuthRequest(Verb.GET, WEIBO_GET_TOKEN_INFO);
+		OAuthRequest getUidRequest = new OAuthRequest(Verb.POST, WEIBO_GET_TOKEN_INFO);
 		this.signRequest(accessToken, getUidRequest);
 		Response response = getUidRequest.send();
+		if(logger.isDebugEnabled()){
+			logger.debug("weibo get user response is: "+response.getBody());
+		}
+		
 		Object result = JSON.parse(response.getBody());
 		
-		User user = new User();		
+		User user = new User();
 		user.setTokenKey(accessToken.getToken());
 		user.setOauthType(getoAuthType());
-		String uid=JSONPath.eval(result, "$.uid").toString();
+		String uid = JSONPath.eval(result, "$.uid").toString();
 		user.setOauthId(uid);
-		
+
 		OAuthRequest getUserRequest = new OAuthRequest(Verb.GET, WEIBO_GET_USER_INFO);
 		getUserRequest.addQuerystringParameter("uid", uid);
 		this.signRequest(accessToken, getUserRequest);
-		Response userInfoResponse = getUidRequest.send();
+		Response userInfoResponse = getUserRequest.send();
+		if(logger.isDebugEnabled()){
+			logger.debug("weibo get user userInfoResponse is: "+userInfoResponse.getBody());
+		}
 		Object userInfo = JSON.parse(userInfoResponse.getBody());
-		
 		user.setOauthName(JSONPath.eval(userInfo, "$.screen_name").toString());
 		user.setUserName(JSONPath.eval(userInfo, "$.screen_name").toString());
 		user.setHeadImgUrl(JSONPath.eval(userInfo, "$.profile_image_url").toString());
-		
+
 		user.setRole(UserRole.USER.toString());
 		return user;
 	}
@@ -67,6 +76,29 @@ public class WeiboOAuthService extends OAuth20ServiceImpl implements CustomOAuth
 	@Override
 	public String getAuthorizationUrl() {
 		return authorizationUrl;
+	}
+
+	@Override
+	public Token getAccessToken(Token requestToken, Verifier verifier) {
+		OAuthRequest request = new OAuthRequest(Verb.POST, api.getAccessTokenEndpoint());
+		request.addQuerystringParameter(OAuthConstants.CLIENT_ID, config.getApiKey());
+		request.addQuerystringParameter(OAuthConstants.CLIENT_SECRET, config.getApiSecret());
+		request.addQuerystringParameter(OAuthConstants.CODE, verifier.getValue());
+		request.addQuerystringParameter(OAuthConstants.REDIRECT_URI, config.getCallback());
+		if (config.hasScope())
+			request.addQuerystringParameter(OAuthConstants.SCOPE, config.getScope());
+		Response response = request.send();
+		Object result = JSON.parse(response.getBody());
+		String access_token = JSONPath.eval(result, "$.access_token").toString();
+		if (!access_token.equals("")) {
+			if(logger.isDebugEnabled()){
+				logger.debug("weibo get user access_token is: "+access_token);
+			}
+			return new Token(access_token, "",response.getBody());
+		}
+
+		return null;
+
 	}
 
 }
