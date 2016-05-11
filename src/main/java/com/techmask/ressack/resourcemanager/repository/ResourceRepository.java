05a10960b9fecc047bcde469423ffbf8211a76b9
edisoncyh <edison_cyh@hotmail.com>
2016-05-11 12:47:20 +0800

    add validation logics for resource add module

--- src/main/java/com/techmask/ressack/resourcemanager/repository/ResourceRepository.java
+++ src/main/java/com/techmask/ressack/resourcemanager/repository/ResourceRepository.java
@@ -42,15 +42,15 @@ public interface ResourceRepository {
 	@Select("SELECT LAST_INSERT_ID()")
 	public long getLastInsertId();
 	
-	@Select("select r.*,u.user_name from resource r,user u where r.status='VALID' and (category=#{category} or 'ALL'=#{category}) and (sub_category=#{subCategory} or 'ALL'=#{subCategory}) and r.owner_id=u.user_id limit #{_startRowIndex}, #{_pageSize}")
+	@Select("select r.*,u.user_name from resource r,user u where r.status='VALID' and (category=#{category} or 'ALL'=#{category}) and (sub_category=#{subCategory} or 'ALL'=#{subCategory}) and r.owner_id=u.user_id order by weight desc, created_date desc limit #{_startRowIndex}, #{_pageSize}")
 	@ResultMap("loadResource-void")
 	public List<Resource> loadAllResource(Map<String,Object> requestMap);
 	
-	@Select("select r.*,u.user_name from resource r,user u where r.status='VALID' and ('ALL'=#{keywords} or MATCH (r.title) AGAINST(#{keywords})) and  r.owner_id=u.user_id limit #{_startRowIndex}, #{_pageSize}")
+	@Select("select r.*,u.user_name from resource r,user u where r.status='VALID' and ('ALL'=#{keywords} or MATCH (r.title) AGAINST(#{keywords})) and  r.owner_id=u.user_id order by weight desc, created_date desc limit #{_startRowIndex}, #{_pageSize}")
 	@ResultMap("loadResource-void")
 	public List<Resource> loadAllResourceByKeywords(Map<String,Object> requestMap);
 
-	@Insert("insert into resource(title,description,category,sub_category,tags,status,orig_url,download_url,download_password,download_times,payment_type,payment_amount,owner_id,created_by,updated_by)values(#{title},#{desc},#{category},#{subCategory},#{tags},'PENDING',#{origUrl},#{downloadUrl},#{downloadPassword},0,'NA',null,#{userId},#{userName},#{userName})")
+	@Insert("insert into resource(title,description,category,sub_category,tags,status,orig_url,download_url,download_password,download_times,payment_type,payment_amount,owner_id,created_by,updated_by)values(#{title},#{desc},#{category},#{subCategory},#{tags},'VALID',#{origUrl},#{downloadUrl},#{downloadPassword},0,'NA',null,#{userId},#{userName},#{userName})")
 	public int addResource(Map<String,Object> resourceMap);
 
 	@Select("select * from resource where resource_id=#{resourceId}")
