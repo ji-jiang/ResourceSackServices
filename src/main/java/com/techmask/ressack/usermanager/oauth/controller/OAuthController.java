2d0103fa319312ea30dff80f70837e44f684b79d
FisherChen <guiku2007@qq.com>
2016-05-13 21:37:21 +0800

    调整注释

--- src/main/java/com/techmask/ressack/usermanager/oauth/controller/OAuthController.java
+++ src/main/java/com/techmask/ressack/usermanager/oauth/controller/OAuthController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.StringUtils;
 import org.scribe.model.Token;
 import org.scribe.model.Verifier;
 import org.slf4j.Logger;
@@ -19,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONPath;
 import com.techmask.ressack.core.busobjs.ResultCode;
 import com.techmask.ressack.core.controller.BaseController;
 import com.techmask.ressack.core.error.ValidationException;
