40fc78852000f6627a81b6908f9250454bd00999
edisoncyh <edison_cyh@hotmail.com>
2016-05-30 12:27:33 +0800

    fix a minor issue

--- src/main/java/com/techmask/ressack/flagmanager/repository/FlagRepository.java
+++ src/main/java/com/techmask/ressack/flagmanager/repository/FlagRepository.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
+
 public interface FlagRepository {
 	public int addFlag(Map<String,Object> resourceMap);
 	public void deleteFlag(Map<String,Object> resourceMap);
